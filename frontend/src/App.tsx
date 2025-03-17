53e80be05c6e1493d53d6c091c89deafe415774a
sw2374 <sw2374@cornell.edu>
2025-03-17 18:50:09 -0400

    fonts and tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,6 +17,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <div className="bg-successLight text-successDark p-4">
+        Success Message!
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
